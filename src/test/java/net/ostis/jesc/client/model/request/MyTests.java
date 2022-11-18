7572c72e79f45972cd719d1f25507414abaa2c4b
Rastsislau Lipski <ungaf@ya.ru>
2022-11-18 12:51:31 +0300

    [ScClient] Implement event handlers

--- src/test/java/net/ostis/jesc/client/model/request/MyTests.java
+++ src/test/java/net/ostis/jesc/client/model/request/MyTests.java
@@ -1,29 +1,5 @@
 package net.ostis.jesc.client.model.request;
 
-import net.ostis.jesc.api.ScApi;
-import net.ostis.jesc.client.ScClient;
-import net.ostis.jesc.client.model.element.ScEventType;
-import net.ostis.jesc.context.ScContextCommon;
-import org.junit.jupiter.api.Test;
-
 class MyTests {
-
-    @Test
-    void isomorphicSubgraphSearchAgent() {
-
-        try (ScClient client = new ScClient("localhost", 8090)) {
-            var api = new ScApi(client);
-            var context = new ScContextCommon(api);
-
-            var ims = context.findBySystemIdentifier("knowledge_base_IMS").get();
-
-            var response = api.events()
-                    .create(ScEventType.ADD_OUTGOING_EDGE, ims)
-                    .execute();
-
-            System.out.println(response);
-        }
-
-    }
 
 }
